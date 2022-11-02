2410b14bb34fe413af4df85e132a4356588deb1a
samsamgo <jku2659@gmail.com>
2022-11-02 23:12:01 +0900

    9095

---
+++ backjjon/9095.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+T = int(input())
+
+
+def sol(n):
+    if n == 1:
+        return 1
+
+    elif n == 2:
+        return 2
+
+    elif n == 3:
+        return 3
+
+    else:
+        return sol(n-1) + sol(n-2) + sol(n-3)
+
+
+for i in range(T):
+    n = int(input())
+    print(sol(n))
