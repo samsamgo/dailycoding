b0dcf0c7a5173482ad6c93c611f0752215e8e2bd
samsamgo <jku2659@gmail.com>
2022-11-07 23:40:26 +0900

    16931.py

--- backjjon/1579.py
+++ backjjon/1579.py
@@ -1,25 +1,27 @@
 import sys
-input = sys.stdin.readline
 
 
 def back_tracking(cnt, idx):
-    if cnt == 1:
-        vo, co = 0,
 
-        for i in range(1):
+  
+    if cnt == l:
+        
+        vo, co = 0, 0
+
+        for i in range(l):
             if answer[i] in consonant:
                 vo += 1
-
             else:
                 co += 1
 
         if vo >= 1 and co >= 2:
             print("".join(answer))
+
         return
 
     for i in range(idx, c):
         answer.append(words[i])
-        back_tracking(cnt+1, i+1)
+        back_tracking(cnt + 1, i + 1)
         answer.pop()
 
 
